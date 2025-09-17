0b7c9049818b98c0daa6dfb3ec84590f7568a3fc
Paribatra30 <batrapari703@gmail.com>
2025-09-17 23:13:15 +0500

    Create Q1.cpp

---
+++ LAB2/Q1.cpp
@@ -0,0 +1,21 @@
+//task no 1 by port botra 24k3114
+#include<iostream>
+using namespace std;
+
+// Function to swap two values
+void SwapValues(int* a, int* b) {
+    if (a == b) return;
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+}
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Before swapping: a = " << a << ", b = " << b << endl;
+    SwapValues(&a, &b);
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+    return 0;
+}
